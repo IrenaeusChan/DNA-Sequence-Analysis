2542172834c6a38d8b50bc9e473573eaa535ceb4
IrenaeusChan <irenaeus.chan@gmail.com>
2015-04-09 12:56:04 -0400

    Changed s_complex.py EvolutionMethod to WeightedEvolutionMethod

--- s_complex.py
+++ s_complex.py
@@ -13,11 +13,22 @@ import string
 import random
 import zlib
 import csv
+import bisect
 from math import log
 
-
-stringSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-'
-#string.ascii_uppercase + string.digits + '-' + '.'+ " " + "#"
+def weightedChoice(choices):
+    """
+    Raymond Hettinger - http://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice
+    """
+    values, weights = zip(*choices)
+    total = 0
+    cum_weights = []
+    for w in weights:
+        total += w
+        cum_weights.append(total)
+    x = random.random()*total
+    i = bisect.bisect(cum_weights, x)
+    return values[i]
 
 def bitCompress(s):
     """
@@ -48,32 +59,31 @@ def entropyInBits(S):
     This is the probability of the character appearing in the String"""
     return -sum([p(s)*log(p(s), 2) for s in N])                     #Find the Entropy Summation for every character seen in the String
 
-def hammingDistance(s1, s2):
-    """
-    Hamming distance.
-    Counts the number of differences between equal lengths of s1 and s2
-    """
-    assert len(s1) == len(s2)
-    ne = operator.ne
-    return sum(imap(ne, s1, s2))
-
 def evolution(s):
     """ 
-    Creates a skeleton string and attempts to randomly build the string.
+    Creates a skeleton string and attempts to randomly build the string using Weighted Averages.
     Tries to build the string Character by Character (Faster)
     by Irenaeus Chan
     """
     if (len(s) == 0):
-        return 0;
-    
+        return 0
     s = s.upper();
+
+    """
+    Creating the Weighted Averages to use for Random Selection
+    """
+    N = {}                                                          #Numerators     Dictionary, of (Key, Value)
+    for char in s:
+        if char not in N: N[char] = 0                               #If the Char is "new". Create a New Spot for it
+        N[char] += 1
+
     skeleton = list(''.join('0') for i in xrange(len(s)));          #Creates the skeleton e.g. GATTACA = 000000
     complexity_count = 0;
     used = [];
 
     for i in xrange(len(skeleton)):
         del used[:]
-        c = ''.join([random.choice(stringSet)]);                    #Randomly chooses a character
+        c = ''.join([weightedChoice(dict.items(N))]);               #Randomly chooses a character from the dictionary
         used.append(c);
     
         while True:
@@ -88,10 +98,11 @@ def evolution(s):
                 skeleton[i] = c;
                 break;              
 
-            c = ''.join([random.choice(stringSet)]);
+            c = ''.join([weightedChoice(dict.items(N))]);
             while (c in used):                                      #Ensures no repeats
-                c = ''.join([random.choice(stringSet)]);
+                c = ''.join([weightedChoice(dict.items(N))]);
             used.append(c);
+    complexity_count = 1.*complexity_count/len(s)
     return complexity_count;                                        #Return how many iterations it took to create the word
 
 def readFile(file_name):
@@ -115,5 +126,5 @@ if __name__ == "__main__":
             #print tags[i][0] + " " + tags[i][1] + " " + tags[i][2]
             #print bitCompress(tags[i][3]), entropyInBits(tags[i][3]), evolution(tags[i][3])
             value = str(bitCompress(tags[i][3])) + " " + str(entropyInBits(tags[i][3])) + " " + str(evolution(tags[i][3])) + "\n"
-            print value
-            output.write(value)
+            print tags[i][0] + " " + tags[i][1] + " " + tags[i][2] + " " + value
+            output.write(tags[i][0] + " " + tags[i][1] + " " + tags[i][2] + " " + value)
